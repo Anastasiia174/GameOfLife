aed07b98ba84c8a97ba26fb49e0d7cb4ecc1478c
Anastasiia174 <potapova174@gmail.com>
2021-04-15 10:15:54 +0300

    Rename main view, add infrastructure

---
+++ GameOfLife.UI/ViewModels/PlaygroundViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameOfLife.UI.Infrastructure;
+
+namespace GameOfLife.UI.ViewModels
+{
+    class PlaygroundViewModel : BindableBase
+    {
+
+    }
+}
---
+++ GameOfLife.UI/Views/Playground.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using GameOfLife.UI.ViewModels;
+
+namespace GameOfLife.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for Playground.xaml
+    /// </summary>
+    public partial class Playground : Window
+    {
+        public Playground()
+        {
+            InitializeComponent();
+
+            DataContext = new PlaygroundViewModel();
+        }
+    }
+}
