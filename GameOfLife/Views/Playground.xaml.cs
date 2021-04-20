61b678ca2b088100e9439334067a08888f4a5076
Anastasiia174 <potapova174@gmail.com>
2021-04-20 15:22:08 +0300

    Add Metro UI to project

--- GameOfLife/Views/Playground.xaml.cs
+++ GameOfLife/Views/Playground.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System.Windows;
 using GalaSoft.MvvmLight.Threading;
+using MahApps.Metro.Controls;
 
 namespace GameOfLife.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
