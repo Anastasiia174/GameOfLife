23db8a326bae728a77f174903d85647d1c5d421e
Anastasiia174 <potapova174@gmail.com>
2021-04-27 21:30:01 +0300

    Add unhandled exceptions catching

--- GameOfLife/App.xaml.cs
+++ GameOfLife/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using GameOfLife.Services;
 
 namespace GameOfLife
 {
@@ -7,5 +10,40 @@ namespace GameOfLife
     /// </summary>
     public partial class App : Application
     {
+        private IDialogService _dialogService;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            _dialogService = new DialogService();
+
+            SetupExceptionHandling();
+        }
+
+        private void SetupExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+                e.SetObserved();
+            };
+        }
+
+        private void LogUnhandledException(Exception exception, string source)
+        {
+            string message = $"Unhandled exception ({source})";
+
+            _dialogService.ShowMessage($"{message}: {exception.Message}");
+        }
     }
 }
