399c5c293d0a843cf65d75be3fe0085c0df6b87c
Anastasiia174 <potapova174@gmail.com>
2021-04-27 22:02:43 +0300

    Add image rescale on mouse wheel

--- GameOfLife/Views/Playground.xaml.cs
+++ GameOfLife/Views/Playground.xaml.cs
@@ -10,6 +10,21 @@ namespace GameOfLife.Views
         public Playground()
         {
             InitializeComponent();
+            Image.MouseWheel += Image_MouseWheel;
+        }
+
+        private void Image_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                ImageScale.ScaleX = ImageScale.ScaleX * 1.10;
+                ImageScale.ScaleY = ImageScale.ScaleY * 1.10;
+            }
+            if (e.Delta < 0)
+            {
+                ImageScale.ScaleX = ImageScale.ScaleX / 1.10;
+                ImageScale.ScaleY = ImageScale.ScaleY / 1.10;
+            }
         }
     }
 }
